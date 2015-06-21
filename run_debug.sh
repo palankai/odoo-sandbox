fa559ae43ec679adfb9e6d887e8be97d0d99360e
Csaba Palankai <csaba.palankai@gmail.com>
2015-06-21 10:23:12 +0100

    Rename the docker image

--- run_debug.sh
+++ run_debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t odoo-swagger-dev .
-docker run -ti --rm -p 8069:8069 -v $(pwd)/addons:/mnt/extra-addons -v $(pwd)/odoo/config:/etc/odoo odoo-swagger-dev openerp-server $@
+docker build -t odoo-swagger-sandbox-dev .
+docker run -ti --rm -p 8069:8069 -v $(pwd)/addons:/mnt/extra-addons -v $(pwd)/odoo/config:/etc/odoo odoo-swagger-sandbox-dev openerp-server $@
